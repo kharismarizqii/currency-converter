2397cd39e59f2d26b7f2b6ce44be656aad4f998f
kharismarizqii <rizqikharisma@gmail.com>
2021-04-10 13:11:23 +0700

    Add: RxJava for reactive programming

--- app/src/main/java/com/kharismarizqii/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/kharismarizqii/currencyconverter/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.kharismarizqii.currencyconverter
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jakewharton.rxbinding2.widget.RxTextView
 import com.kharismarizqii.currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val beforeStream = RxTextView.textChanges(binding.etBefore)
+            .skipInitialValue()
+            .map {
+                it.toString()
+            }
+
+        beforeStream.subscribe{
+            binding.etAfter.setText(it)
+        }
     }
 }
